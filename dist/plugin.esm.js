551a997fac5128d716d6da01dab53f1490c9dd8f
Jake Krammer <jake@savvytrader.com>
2025-07-21 11:14:31 -0700

    Commit dist

---
+++ dist/plugin.esm.js
@@ -0,0 +1,52 @@
+import { registerPlugin, WebPlugin } from '@capacitor/core';
+
+const MediaSession = registerPlugin('MediaSession', {
+    web: () => Promise.resolve().then(function () { return web; }).then(m => new m.MediaSessionWeb()),
+    ios: () => Promise.resolve().then(function () { return web; }).then(m => new m.MediaSessionWeb())
+});
+
+class MediaSessionWeb extends WebPlugin {
+    async setMetadata(options) {
+        if ('mediaSession' in navigator) {
+            navigator.mediaSession.metadata = new MediaMetadata(options);
+        }
+        else {
+            throw this.unavailable('Media Session API not available in this browser.');
+        }
+    }
+    async setPlaybackState(options) {
+        if ('mediaSession' in navigator) {
+            navigator.mediaSession.playbackState = options.playbackState;
+        }
+        else {
+            throw this.unavailable('Media Session API not available in this browser.');
+        }
+    }
+    ;
+    async setActionHandler(options, handler) {
+        if ('mediaSession' in navigator) {
+            navigator.mediaSession.setActionHandler(options.action, handler);
+        }
+        else {
+            throw this.unavailable('Media Session API not available in this browser.');
+        }
+    }
+    ;
+    async setPositionState(options) {
+        if ('mediaSession' in navigator) {
+            navigator.mediaSession.setPositionState(options);
+        }
+        else {
+            throw this.unavailable('Media Session API not available in this browser.');
+        }
+    }
+    ;
+}
+
+var web = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    MediaSessionWeb: MediaSessionWeb
+});
+
+export { MediaSession };
+//# sourceMappingURL=plugin.esm.js.map
